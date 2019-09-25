0992d309295bda69a3f75ea37a15d0c53cfba257
Ivelin Penchev <ivelinpenchev@gmail.com>
2019-09-25 20:51:51 +0100

    Updated tests

--- mapaccess_test.go
+++ mapaccess_test.go
@@ -1,15 +1,23 @@
 package mapaccess
 
 import (
-	"encoding/json"
 	"reflect"
 	"testing"
 )
 
+var data = map[string]interface{}{
+	"array": []interface{}{"value"},
+	"one":   "two",
+	"nested": map[string]interface{}{
+		"key":   "three",
+		"array": []interface{}{"four"},
+	},
+}
+
 func TestGet(t *testing.T) {
 	type args struct {
 		key  string
-		data []byte
+		data interface{}
 	}
 	tests := []struct {
 		name    string
@@ -17,22 +25,20 @@ func TestGet(t *testing.T) {
 		want    interface{}
 		wantErr bool
 	}{
-		{"root", args{"one", []byte(`{ "one": "value" }`)}, "value", false},
-		{"root array", args{"single[0]", []byte(`{ "single": [ "value" ] }`)}, "value", false},
-		{"nested", args{"one.two", []byte(`{ "one": {"two": "value"} }`)}, "value", false},
-		{"nested array", args{"one.two[0]", []byte(`{ "one": {"two": [ "value" ]} }`)}, "value", false},
+		{"root", args{"one", data}, "two", false},
+		{"root array", args{"array[0]", data}, "value", false},
+		{"nested", args{"nested.key", data}, "three", false},
+		{"nested array", args{"nested.array[0]", data}, "four", false},
 
-		{"spaces", args{" one.two[0]", []byte(`{ "one": {"two": [ "value" ]} }`)}, nil, true},
-		{"spaces", args{"o.test.", []byte(`{ "one": {"two": [ "value" ]} }`)}, nil, true},
-		{"spaces", args{"[0]", []byte(`[ "value" ]`)}, nil, true},
-		{"nested array missing", args{"one.two[1]", []byte(`{ "one": {"two": [ "value" ]} }`)}, nil, true},
-		{"root missing", args{"two", []byte(`{"one": "value"}`)}, nil, true},
+		{"spaces", args{" one.two[0]", data}, nil, true},
+		{"spaces", args{"o.test.", data}, nil, true},
+		{"spaces", args{"[0]", data}, nil, true},
+		{"nested array missing", args{"one.two[1]", data}, nil, true},
+		{"root missing", args{"two", data}, nil, true},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			var d interface{}
-			err := json.Unmarshal(tt.args.data, &d)
-			got, err := Get(tt.args.key, d)
+			got, err := Get(tt.args.key, tt.args.data)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Get() error = %v, wantErr %v", err, tt.wantErr)
 				return
